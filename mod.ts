e7dbc57cd311a3a298dca5e271308660a5584983
skanehira <sho19921005@gmail.com>
2021-06-10 08:53:42 +0900

    fix uncatched error

--- mod.ts
+++ mod.ts
@@ -4,34 +4,59 @@ import { parseText } from "./util.ts";
 const endpoint =
   "https://script.google.com/macros/s/AKfycbywwDmlmQrNPYoxL90NCZYjoEzuzRcnRuUmFCPzEqG7VdWBAhU/exec";
 
+interface Body {
+  // deno-lint-ignore camelcase
+  response_type: string;
+  attachments: [{
+    color?: string;
+    pretext: string;
+    text?: string;
+    footer?: string;
+  }];
+}
+
 serve({
   "/": async (request) => {
+    const body = {
+      "response_type": "in_channel",
+    } as Body;
+
     const form = await request.formData();
     const text = form.get("text") as string;
     if (!text) {
-      const resp = {
-        "response_type": "in_channel",
-        "text": "text is empty",
-      };
-      return new Response(JSON.stringify(resp));
-    }
-    const ctx = parseText(text);
-    const resp = await fetch(endpoint, {
-      method: "POST",
-      body: JSON.stringify(ctx),
-    });
-
-    const body = {
-      "response_type": "in_channel",
-      "attachments": [
+      body.attachments = [
         {
-          "color": "#36a64f",
-          "pretext": ctx.text,
-          "text": await resp.text(),
-          "footer": "https://cloud.google.com/translate/docs/languages"
+          color: "#D84646",
+          pretext: "text is empty",
         },
-      ],
-    };
+      ];
+      return new Response(JSON.stringify(body));
+    }
+
+    try {
+      const ctx = parseText(text);
+      const resp = await fetch(endpoint, {
+        method: "POST",
+        body: JSON.stringify(ctx),
+      });
+
+      body.attachments = [
+        {
+          color: "#36a64f",
+          pretext: ctx.text,
+          text: await resp.text(),
+          footer: "https://cloud.google.com/translate/docs/languages",
+        },
+      ];
+    } catch (e) {
+      body.attachments = [
+        {
+          color: "#D84646",
+          pretext: e.toString(),
+        },
+      ];
+    }
+
     return new Response(JSON.stringify(body), {
       headers: {
         "Content-Type": "application/json",
